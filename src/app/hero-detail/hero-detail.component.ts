980e45f91cc3409cc3c0bdad45cc3b3f19319a71
jojomango <q9898989@gmail.com>
2020-09-05 21:41:46 +0800

    http, search

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,6 +28,11 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+    .subscribe(() => this.goBack());
+  }
+
   ngOnInit(): void {
     this.getHero();
   }
